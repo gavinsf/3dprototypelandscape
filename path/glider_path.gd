1102d192800deca7300e37c3ca71f12e373a8d5e
Jacob <winkschonewille@gmail.com>
2024-10-26 18:44:10 -0700

    implemented SFC algorithm

--- path/glider_path.gd
+++ path/glider_path.gd
@@ -4,14 +4,14 @@ extends Path3D
 
 
 # MEMBERS ######################################################################
-var DRAW_HILBERT = true
-var HEIGHT = 15.0
+@export var DRAW_HILBERT_CURVE = true
+var HEIGHT = 14
 
 # VIRTUALS #####################################################################
 func _ready() -> void:
-	var hilbert_curve : Curve2D = create_hilbert_curve2d(3)
-	print(hilbert_curve.get_point_count())
-	#if DRAW_HILBERT: 
+	var hilbert_curve : Curve2D = create_hilbert_curve2d(8)
+	if DRAW_HILBERT_CURVE: 
+		draw_lines_from_curve_array(hilbert_curve, HEIGHT)
 	#for _point in hilbert_curve: print(_point)
 
 
@@ -21,15 +21,16 @@ func create_hilbert_curve2d(order : int) -> Curve2D:
 	# Define Hilbert curve L-System 
 	var _alphabet = {"productions":["A","B"], "constants":["F","+","-"]} # V
 	var _axiom = "A" # w
-	var _productions = {"A":"+BF−AFA−FB+", "B":"−AF+BFB+FA−"} # P
+	var _productions = {"A":"+BF-AFA-FB+", "B":"-AF+BFB+FA-"} # P
 	
 	# Create L-System Components
-	var _curve = Curve2D.new()
+	var _curve : Array[Curve2D]= [] 
 	var _l_system_str : String = _axiom
+	
+	_curve.push_front(Curve2D.new()) # array reference makes it passable
 	
 	# Iterate to make specific order Hilbert L-System string
 	for _current_iteration in order:
-		print("Iteration " + str(order) + ", start: " + _l_system_str)
 		var _constructed_string = ""
 		
 		# Apply productions to current L-System iteraton.
@@ -44,31 +45,69 @@ func create_hilbert_curve2d(order : int) -> Curve2D:
 					break;
 		
 		_l_system_str = _constructed_string
-		print("Iteration " + str(order) + ", finished: " + _l_system_str)
 	
 	# Use string-encoded L-System string to guide the progress of a curve3D
-	var _path_direction = Vector2(1,0) # Vector starts in +X direction
+	var _path_direction : Vector2 = Vector2(1,0) # Vector starts in +X direction
+	
 	# First point...
-	_curve.add_point(Vector2.ZERO, Vector2.ZERO, _path_direction)
+	_curve[0].add_point(Vector2.ZERO, Vector2.ZERO, _path_direction)
 	# ...create the rest.
 	for _symbol in _l_system_str:
 		match _symbol:
-			"A": break
-			"B": break
-			"F": set_next_lsystem_point(_path_direction, _curve)
-			"+": _path_direction.rotated(-PI/2) # anti-clockwise 90deg
-			"-": _path_direction.rotated(PI/2)	# clockwise 90deg
-	
-	return _curve
+			"A": 
+				continue
+			"B": 
+				continue
+			"F": 
+				set_next_lsystem_point(_path_direction, _curve[0])
+			"+": 
+				_path_direction = _path_direction.rotated(PI/2) # anti-clockwise 90deg
+			"-": 
+				_path_direction = _path_direction.rotated(-PI/2)	# clockwise 90deg
+		
+	return _curve[0]
 
 # Inserts a point on a curve
-func set_next_lsystem_point(direction:Vector2, curve:Curve2D) -> void:
-	var _point_count = curve.get_point_count()
-	var _point_pos = curve.get_point_position(_point_count - 1) + direction
+func set_next_lsystem_point(direction:Vector2, ref_curve:Curve2D) -> void:
+	var _point_count = ref_curve.get_point_count()
+	var _point_pos = ref_curve.get_point_position(_point_count - 1) + direction
 	
+	if _point_count >= 1:
+		ref_curve.set_point_out(_point_count-1, direction)
 	
-	curve.add_point(_point_pos, -curve.get_point_out(_point_count-1), direction)
+	ref_curve.add_point(_point_pos, -direction, Vector2.ZERO)
 	return
 
-func draw_line(position_1 : Vector3, position_2 : Vector3) -> void:
-	pass
+func draw_lines_from_curve_array(curve_ : Curve2D, height : float) -> void:
+	var _mesh = MeshInstance3D.new()
+	var _surface_tool = SurfaceTool.new()
+	var _material = StandardMaterial3D.new()
+	_material.vertex_color_use_as_albedo = true
+	var _previous_point = Vector2.ZERO
+	var point_tally = -1
+	
+	_surface_tool.begin(Mesh.PRIMITIVE_LINES)
+	var skip_flag = true
+	for _point in curve_.get_baked_points():
+		
+		# guard clause
+		if skip_flag:
+			_previous_point = _point
+			skip_flag = false
+			continue
+		
+		_surface_tool.set_color(Color(1.0, 0.01, 0.01, 1.0))
+		_surface_tool.add_vertex(Vector3(_previous_point.x, height, _previous_point.y))
+		_surface_tool.set_color(Color(1.0, 0.01, 0.01, 1.0))
+		_surface_tool.add_vertex(Vector3(_point.x, height, _point.y))
+		
+		point_tally += 2
+		_surface_tool.add_index(point_tally - 1)
+		_surface_tool.add_index(point_tally)
+		
+		_previous_point = _point
+	
+	_mesh.mesh = _surface_tool.commit()
+	_mesh.material_override = _material
+	
+	add_child(_mesh)
